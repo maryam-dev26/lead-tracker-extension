5b3d3c5b76c1b20223627d2356913438e133ee18
Maryam <masumaakhtermaryam@gmail.com>
2026-07-31 07:36:16 +0600

    checking localStorage

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const ulEl = document.getElementById("ul-el")
 // Get the leads from the localStorage - PS: JSON.parse()
 // Store it in a variable, leadsFromLocalStorage
 // Log out the variable
-localStorage.clear()
+
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 console.log(leadsFromLocalStorage)
+
+// 1. Check if leadsFromLocalStorage is truthy
+// 2. If so, set myLeads to its value and call renderLeads()
+if (leadsFromLocalStorage){
+    myLeads = leadsFromLocalStorage
+    renderLeads()
+}
 
 inputBtn.addEventListener("click", function(){
     myLeads.push(inputEl.value)
@@ -32,3 +39,20 @@ for (let i = 0; i < myLeads.length; i++) {
 }
 ulEl.innerHTML = listItems
 }
+
+
+
+// 6 falsy values in javascript
+// false
+// 0
+// ""
+// null -> how you as a developer signalize emptiness
+// undefined -> how JavaScript signalizes emptiness
+// NaN
+
+console.log(  Boolean("")   ) // false
+console.log(  Boolean("0")  ) // true
+console.log(  Boolean(100)  ) // true
+console.log(  Boolean(null) ) // false
+console.log(  Boolean([0])  ) // true
+console.log(  Boolean(-0)   ) // false
