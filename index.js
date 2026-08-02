23a1d465bcf2f19f21939c7b229b04bb70bf4130
Maryam <masumaakhtermaryam@gmail.com>
2026-08-02 08:10:29 +0600

    little correction

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ if (leadsFromLocalStorage){
     render(myLeads)
 }
 
-tabBtn.addEventListener("click", function() {
+tabBtn.addEventListener("click", function(tabs) {
 //chrome dot tabs, that's a so called API application programming interface.
     chrome.tabs.query({active: true, currentWindow: true}, function(){
         myLeads.push(tabs[0].url)
